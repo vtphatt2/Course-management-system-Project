1e1cb8b5f85722eef87cb15320e47d9c77db399e
Luong Nguyen Khoa <cucdaunho@gmai.com>
2023-03-18 22:32:36 +0700

    create a new school year

---
+++ Function/Staff/staff.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <sys/stat.h>
+#include "../Staff/staff.h"
+
+using namespace std;
+
+const string schoolYears = "DataSet/SchoolYear/existSchoolYear/txt";
+
+void staff_view_all_school_years(); // view all school years
+void staff_edit(); // create or delete a new school year
+void staff_create_a_new_school_year(); // create a new school year
+void staff_delete_a_school_year(); // delete a school year
