670ac1f49adb10fb66b07c5bb542bd11f35762c3
Thinh Phat <vothinhphat@VoThinhPhat.local>
2023-03-27 15:11:00 +0700

    Add std::string

--- Function/General/Registration/registration.h
+++ Function/General/Registration/registration.h
@@ -1,6 +1,6 @@
-const string accountFile = "DataSet/accountUser.txt";
-const string taskStudent = "Function/General/Registration/taskOfStudent.txt";
-const string taskStaff = "Function/General/Registration/taskOfStaff.txt";
+const std::string accountFile = "DataSet/accountUser.txt";
+const std::string taskStudent = "Function/General/Registration/taskOfStudent.txt";
+const std::string taskStaff = "Function/General/Registration/taskOfStaff.txt";
 
 void login(std::string &idUser, bool &logingIn); //login to Moodle
 void task(std::string idUser, bool &logingIn); //to show all the task that user can do
