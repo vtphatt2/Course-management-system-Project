29c701315e611601e22bf345ff5a11fb860c9813
TienWjro <thanhtienst2004@gmail.com>
2023-03-27 10:31:09 +0700

    Delete redundant

--- Function/Student/student.h
+++ Function/Student/student.h
@@ -3,9 +3,4 @@
 #include <string>
 using namespace std;
 
-struct ClassNode {
-    string className;
-    ClassNode* next;
-};
-void readClassesFromFile(string fileName);
 void viewCourses(string id); //View a list of 1 student's courses
