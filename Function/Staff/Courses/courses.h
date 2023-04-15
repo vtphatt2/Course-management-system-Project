ef2f2c5afee2d20f7f7894b79fa3def1fdec6429
ndtho22 <ndt280504@gmail.com>
2023-04-15 14:26:03 +0700

    Add student to Course

--- Function/Staff/Courses/courses.h
+++ Function/Staff/Courses/courses.h
@@ -34,3 +34,4 @@ void changeNumCredits(string &existSemester, string &year, string &year_semester
 void changeSession(string &existSemester, string &year, string &year_semester, string &semester, int &order, string &course); // change session
 void changeTeacher(string &existSemester, string &year, string &year_semester, string &semester, int &order, string &course); // change teacher
 void changeMaximumStudents(string &existSemester, string &year, string &year_semester, string &semester, int &order, string &course); // change maximum of students
+void addStudentToCourse(string &existSemester, string &semester, string &year,string &year_semester, string &course, int &order); // add student to course
