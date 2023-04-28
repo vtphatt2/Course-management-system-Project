606ead85dff41914f807dc54065e7c2baeda3b24
Luong Nguyen Khoa <cucdaunho@gmai.com>
2023-04-28 15:53:31 +0700

    Add delete schoolyear function

--- Function/Staff/SchoolYears/editSchoolYears.cpp
+++ Function/Staff/SchoolYears/editSchoolYears.cpp
@@ -1,11 +1,14 @@
+#include <iostream>
+#include <cstdlib>
 #include "../staff.h"
 
 void editSchoolYears() {
+    createTitle("EDIT");
     cout << '\n';
     cout << "1. Create a new school year" << '\n';
     cout << "2. Delete a school year" << '\n';
     cout << "3. Back" << '\n';
-    cout << "Your choose is: ";
+    cout << "Your choose is : ";
     int choose;
     cin >> choose;
     if (choose == 1) staff_create_a_new_school_year(); // create a new school year
@@ -47,5 +50,55 @@ void staff_create_a_new_school_year() {
 }
 
 void staff_delete_a_school_year() {
-    // meow meow
+    string existSchoolYear_tmp = "DataSet/SchoolYear/existSchoolYear.txt";
+    const char* existSchoolYear = existSchoolYear_tmp.c_str();
+    string s;
+    
+    ifstream in;
+    in.open(existSchoolYear);
+    int numberOfSchoolYear = 0;
+    while (getline(in, s)) ++numberOfSchoolYear;
+    in.close();
+
+    string* arr = new string[numberOfSchoolYear];
+    int t = 0;
+    in.open(existSchoolYear);
+    while (getline(in, s)) {
+        // take the school year
+        arr[t++] = s;
+    }
+    in.close();
+
+    cout << "\n* Choose School Year that you want to remove :" << '\n';
+    for (int i = 0; i < numberOfSchoolYear; ++i) cout << i + 1 << ". " << arr[i] << '\n';
+    
+    cout << "\nYour choice is : ";
+    int choice;
+    cin >> choice;
+
+    string folder_path = "DataSet/SchoolYear/" + arr[choice-1];
+    string command = "rm -rf \"" + folder_path + "\"";
+    system(command.c_str());
+
+    string test_tmp = "DataSet/SchoolYear/test.txt";
+    const char* test = test_tmp.c_str();
+    ofstream testFile(test);
+    for (int i = 0; i < numberOfSchoolYear; ++i) {
+        if (i == choice - 1) continue;
+        else {
+            testFile << arr[i] << '\n';
+        }
+    }
+
+    testFile.close();
+    remove(existSchoolYear);
+    rename(test, existSchoolYear);
+
+    delete[] arr;
+
+    cout << "\nREMOVE SUCCESSFULLY !";
+    cout << "\nType any key to back : ";
+    string ans;
+    cin >> ans;
+    schoolYearAndEdit();
 }
