71dc37776957a4157f78f083e3d3cb2c26cfa484
TienWjro <thanhtienst2004@gmail.com>
2023-05-07 08:09:59 +0700

    Complete function ViewCourses (Update)

--- Function/Student/student.h
+++ Function/Student/student.h
@@ -1,8 +1,9 @@
 #pragma once
 #include <iostream>
-
+#include <fstream>
+#include <string.h>
 using namespace std;
 
-void viewCourses(string id); //View a list of 1 student's courses
-void chooseSchoolYear(); // choose schoolyear
-void chooseSemester(string &year); // choose semester
+void viewCourses(string id, string &year, string &semester); //View a list of 1 student's courses
+void chooseSchoolYear(string id); // choose schoolyear
+void chooseSemester(string id, string &year); // choose semester
--- Function/Student/viewCourses.cpp
+++ Function/Student/viewCourses.cpp
@@ -1,18 +1,45 @@
-#include <iostream>
-#include <fstream>
-#include <cstring>
-using namespace std;
-
+#include "student.h"
+void printName(string s, string address){
+    string addressTmp;
+    ifstream fin;
+    s = "Course ID: " + s;
+    for (int i=1; i<=4; i++){
+        if (i==1) addressTmp = address + "1stYear/courseInfo.txt";
+        if (i==2) addressTmp = address + "2ndYear/courseInfo.txt";
+        if (i==3) addressTmp = address + "3rdYear/courseInfo.txt";
+        if (i==4) addressTmp = address + "4thYear/courseInfo.txt";
+        fin.open(addressTmp);
+        string str;
+        while (getline(fin,str)){
+            if (str == s){
+                getline(fin,str);
+                getline(fin,str);
+                cout << str << '\n';
+                return;
+            }
+        }
+        fin.close();
+    }
+    cout << "Course Name: Could not find data\n";
+}
 void viewCourses(string id, string &year, string &semester){
+    cout << "MY COURSES: \n\n"; // create Title
+    string addressCourses = "DataSet/SchoolYear/" + year + "/" + semester + "/";
     string address = "DataSet/InfoStudent/" + id + "/courses.txt";
     string s;
     ifstream fin;
     fin.open(address);
-    int flag=0;
+    int flag=0,cnt=0;
     while (getline(fin,s)){
         if (flag==2){
-            cout << s << '\n';
-            if (s=="") flag=0; 
+            if (s=="") {
+                flag=0;
+                continue;
+            } 
+            cnt++;
+            cout << cnt << ". " << s << '\n';
+            printName(s,addressCourses);
+            cout << '\n';
         }
         if (s==year) flag=1;
         if (flag==1 && s==semester) flag=2;
